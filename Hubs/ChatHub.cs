c37e6c51df8ccca7ddce9bb280e13617ae8938bf
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2020-09-09 08:39:55 +0300

    Implementing SignalR for chat functionality

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,8 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace ReaiotBackend.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task GroupMessage(string name, string message, string room)
+        {
+            await Clients.Group(room).SendAsync(name, message);
+        }
+        public Task JoinRoom(string roomName)
+        {
+            return  Groups.AddToGroupAsync(Context.ConnectionId ,roomName);
+        }
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
     }
 }
