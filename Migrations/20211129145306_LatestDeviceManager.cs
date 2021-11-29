39337a639a114ba31f50e9acf88f682e682d5e56
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-11-29 17:53:41 +0300

    added latest change migrations

---
+++ Migrations/20211129145306_LatestDeviceManager.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReaiotBackend.Migrations
+{
+    public partial class LatestDeviceManager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ACC_X",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "ACC_Y",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "ACC_Z",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "GeophoneAnalogValue",
+                table: "DeviceMessage");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Geophone_analog_value",
+                table: "DeviceMessage",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "X_acc",
+                table: "DeviceMessage",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Y_acc",
+                table: "DeviceMessage",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Z_acc",
+                table: "DeviceMessage",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Geophone_analog_value",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "X_acc",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "Y_acc",
+                table: "DeviceMessage");
+
+            migrationBuilder.DropColumn(
+                name: "Z_acc",
+                table: "DeviceMessage");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ACC_X",
+                table: "DeviceMessage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ACC_Y",
+                table: "DeviceMessage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ACC_Z",
+                table: "DeviceMessage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GeophoneAnalogValue",
+                table: "DeviceMessage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
